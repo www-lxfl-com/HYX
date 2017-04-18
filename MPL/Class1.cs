ae3f7d01c10f4fd23541c1ccbd703d554e89d758
Jaya <Jaya@JAYAS>
2017-04-18 19:05:29 +0800

    HYX

---
+++ MPL/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MPL
+{
+    public class Class1
+    {
+    }
+}
